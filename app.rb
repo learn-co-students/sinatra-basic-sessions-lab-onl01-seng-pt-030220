71abc21f610638704204d51133d539078d04c546
8bitbran <brandonmar97@gmail.com>
2020-06-01 23:06:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,19 @@ class App < Sinatra::Base
         erb :index
     end 
 
+    get '/set' do
+        session[:foo] = 'hello'
+    if session[:foo] == 'hello'
+      redirect '/fetch'
+    else
+      "Session value has not been set!"
+    end
+    end 
+
+    get '/fetch' do 
+        "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
+    end 
+
     post '/checkout' do
         session[:item] = params[:item]
         @session = session
